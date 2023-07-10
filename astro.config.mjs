5982b252832521a10a89f64b2f4fcecff49633dc
Nguyen Hoang Long <long.nguyen2706@hcmut.edu.vn>
2023-07-10 09:24:43 +0700

    fix: add something

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from '@astrojs/vercel/static';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'static',
-  adapter: vercelStatic(),
+
 });
